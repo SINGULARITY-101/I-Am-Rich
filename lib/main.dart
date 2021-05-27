72380f03fb4f8ed661e6519bb7fa0ea237a86cd6
SINGULARITY <49837654+SINGULARITY-101@users.noreply.github.com>
2021-05-27 20:50:47 +0530

    Commiting

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage("images/diamond.png"),
+          ),
+        ),
+        appBar: AppBar(
+          title: Text("I am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    ),
+  );
+}
